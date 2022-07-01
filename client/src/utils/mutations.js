a61f8669eabb23410b056d8815c47b92216cd7d4
rtleib <rtleib@gmail.com>
2022-06-30 19:44:55 -0500

    added mutations.js in client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,56 @@
+import { gql } from '@apollo/client';
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String!]
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String!
+    $link: String!
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+    ) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
